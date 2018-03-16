a0281ddc99e126178dbace6f4945dd6fdd2ab0d7
Namit <namit2saxena@gmail.com>
2018-03-16 13:55:37 +0530

    First commit

---
+++ ICommandBasic/ViewModels/ViewModelBase.cs
@@ -0,0 +1,24 @@
+﻿using ICommandBasic.ViewModels.Commands;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ICommandBasic.ViewModels
+{
+    public class ViewModelBase
+    {
+        public SimpleCommand SimpleCommand { get; set; }
+
+        public ViewModelBase()
+        {
+            this.SimpleCommand = new SimpleCommand(this);
+        }
+        public void SimpleMethod()
+        {
+            Debug.WriteLine("Hello");
+        }
+    }
+}
